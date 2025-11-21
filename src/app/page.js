43172dfc2633cdf284d4442c238cfa88540bd600
Washington <washington.contato1327@gmail.com>
2025-11-21 18:04:11 -0300

    Atualiza estilos globais e página inicial
    - Ajusta src/app/globals.css com novos estilos
    - Atualiza layout e conteúdo da página inicial (src/app/page.js)

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,33 @@ import Filter from "@/components/filter/Filter";
 
 export default function Home() {
   return (
-    <main className="home">
-      <aside>
-        <Filter />
-      </aside>
-      <section>
-        <Banner />
-      </section>
+    <main className="home-page_container">
+      <div className="home-page_content">
+        {/* Banner principal */}
+        <div className="banner-wrapper">
+          <Banner />
+        </div>
+        {/* Conteúdo principal */}
+        <div className="content-layout ">
+          {/* Filtro lateral */}
+          <aside className="sidebar ">
+            <Filter />
+          </aside>
+
+          {/* Seção principal */}
+          <section className="main-content  grid grid-cols-3 max-xl:grid-cols-2 gap-2">
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+            <div className="border h-[300px]"></div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
