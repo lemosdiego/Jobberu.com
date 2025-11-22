3beb37842743fcd0203e94c9da573aca7fd3b2ea
Washington <washington.contato1327@gmail.com>
2025-11-22 07:12:37 -0300

    Atualiza estilos globais e página principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,14 @@
 import Banner from "@/components/banner/Banner";
+import SectionProfessionals from "@/components/card/SectionProfessionals";
 import Filter from "@/components/filter/Filter";
 
 export default function Home() {
   return (
     <main className="home-page_container">
-      <div className="home-page_content">
-        {/* Banner principal */}
-        <div className="banner-wrapper">
-          <Banner />
-        </div>
-        {/* Conteúdo principal */}
-        <div className="content-layout ">
-          {/* Filtro lateral */}
-          <aside className="sidebar ">
-            <Filter />
-          </aside>
-
-          {/* Seção principal */}
-          <section className="main-content  grid grid-cols-3 max-xl:grid-cols-2 gap-2">
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-            <div className="border h-[300px]"></div>
-          </section>
-        </div>
+      <Banner />
+      <div className="content-layout ">
+        <Filter />
+        <SectionProfessionals />
       </div>
     </main>
   );
