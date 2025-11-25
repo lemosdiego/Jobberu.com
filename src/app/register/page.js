63505279c3a3bd9e197ad2dcfb8e22e48c3b3235
Washington <washington.contato1327@gmail.com>
2025-11-24 21:14:47 -0300

    Atualiza página de registro, adiciona melhorias visuais e inclui imagens públicas

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,3 +1,36 @@
+"use client";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+
 export default function Register() {
-  return <main>Página de registro</main>;
+  const params = useSearchParams();
+  const type = params.get("type");
+
+  const isProfissional = type === "profissional";
+
+  return (
+    <main
+      className={` grid [grid-template-columns:4fr_3fr] min-h-[90vh] ${
+        isProfissional ? "bg-blue-200" : " bg-white"
+      }`}
+    >
+      <div className="relative">
+        {
+          <Image
+            src={`${isProfissional ? "/profissionais.jpg" : "/clientes.jpg"}`}
+            alt="Icon"
+            fill
+            objectFit="cover"
+          />
+        }
+      </div>
+      <div className=" flex flex-col items-center justify-center p-20">
+        <h2>
+          {isProfissional
+            ? "Registrar como Profissional"
+            : "Registrar como Cliente"}
+        </h2>
+      </div>
+    </main>
+  );
 }
