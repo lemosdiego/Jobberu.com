1df96a885488c6ef3bdce8b5ce6892faa638a803
Washington <washington.contato1327@gmail.com>
2025-11-29 20:38:15 -0300

    Refatora estrutura do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 // /app/page.js (ou o arquivo da sua Home)
-"use client"; // Adicione esta diretiva no topo!
+"use client";
+import Banner from "@/components/site/Banner/Banner";
+
+// Adicione esta diretiva no topo!
 
 export default function Home() {
-  return <main className="home-page_container"></main>;
+  return (
+    <main className="home-page_container">
+      <Banner />
+    </main>
+  );
 }
