bb237f4e229f160ad3ad35549352bec15f16bf43
Washington <washington.contato1327@gmail.com>
2025-12-18 13:43:40 -0300

    Adiciona seção sobre, ajustes nos estilos

--- src/app/sobre-nos/page.js
+++ src/app/sobre-nos/page.js
@@ -1,9 +1,18 @@
+import About from "@/components/sections/About/About";
 import Banner from "@/components/sections/Banner/Banner";
+import Benefits from "@/components/sections/Benefits/Benefits";
+import Categories from "@/components/sections/categories/Categories";
 
 export default function AboutPage() {
   return (
     <main>
-      <Banner />
+      <Banner
+        title="Feita para a Comunidade"
+        description="A Jobberu foi criada para fortalecer a comunidade, conectando profissionais locais a clientes que buscam serviços com confiança e facilidade."
+      />
+      <About />
+      <Categories />
+      <Benefits />
     </main>
   );
 }
---
+++ src/components/sections/About/About.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import "./About.css";
+
+export default function About() {
+  return (
+    <section className="section-about">
+      <div className="section-about_container">
+        <div className="section-about_content">
+          <h2 className="section-about_content-title">Sobre a Jobberu</h2>
+          <p className="section-about_content-description">
+            A Jobberu nasceu com o propósito de facilitar a vida das pessoas
+            através da tecnologia, conectando quem precisa de um serviço a quem
+            sabe fazê-lo com excelência. Seja para reparos domésticos, cuidados
+            pessoais ou serviços especializados, nossa plataforma oferece uma
+            maneira rápida e segura de encontrar o profissional ideal.
+          </p>
+          <p className="section-about_content-description">
+            Valorizamos a confiança e a qualidade. Por isso, criamos um espaço
+            onde prestadores de serviço podem divulgar seu trabalho de forma
+            profissional e clientes podem contratar com tranquilidade. Junte-se
+            a nós e descubra como a Jobberu pode transformar o seu dia a dia.
+          </p>
+        </div>
+        <div className="section-about_container-image">
+          <Image src={"/about.jpg"} alt="Sobre nós" fill objectFit="cover" />
+        </div>
+      </div>
+    </section>
+  );
+}
