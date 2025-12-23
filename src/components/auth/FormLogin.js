ad3759756028a326e2b1af9dc78764cbcaf1a397
Washington <washington.contato1327@gmail.com>
2025-12-23 11:30:41 -0300

    Inicia refatoração, separando responsabilidades em chamadas para api em hooks separados, iniciando pelo useLogin

--- src/components/auth/FormLogin.js
+++ src/components/auth/FormLogin.js
@@ -1,68 +1,20 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import api from "@/services/api";
-import { useAuth } from "@/context/AuthContext";
 import Link from "next/link";
 import "./Auth.css";
+import { useLogin } from "@/hooks/useLogin"; // ajuste o path conforme sua estrutura
 
 export default function FormLogin() {
-  const [formData, setFormData] = useState({
-    email: "",
-    senha: "",
-  });
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  const { login } = useAuth();
-  const searchParams = useSearchParams();
-  const redirectUrl = searchParams.get("redirect");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setLoading(true);
-
-    try {
-      const response = await api.post("/usuario/login", formData);
-      const { token, usuario } = response.data;
-
-      // Usa a função do nosso AuthContext para gerenciar o estado
-      login(usuario, token);
-
-      // Se houver uma URL de redirecionamento, usa ela.
-      if (redirectUrl) {
-        router.push(redirectUrl);
-      } else {
-        // Caso contrário, redireciona com base no tipo de usuário.
-        const destination = usuario.is_prestador ? "/dashboard" : "/";
-        router.push(destination);
-      }
-    } catch (err) {
-      console.error("Erro no login:", err);
-      setError(
-        err.response?.data?.error || "Ocorreu um erro. Tente novamente."
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { formData, error, loading, handleChange, handleSubmit } = useLogin();
 
   return (
     <form onSubmit={handleSubmit} className="form-login">
       <div className="form-register_container-title">
         <h2 className="form-login_title">Acessar sua conta</h2>
       </div>
+
       {error && <p className="form-login_error">{error}</p>}
+
       <div className="flex flex-col">
         <label htmlFor="email" className="form-login_label">
           Email
@@ -96,6 +48,7 @@ export default function FormLogin() {
       <button type="submit" disabled={loading} className="form-login_button">
         {loading ? "Entrando..." : "Entrar"}
       </button>
+
       <div className="form-login_register">
         <p className="form-login_register_text">
           Não tem uma conta? Cadastre-se como
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,70 @@
+"use client";
+
+import { useAuth } from "@/context/AuthContext";
+import api from "@/services/api";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export function useLogin() {
+  const [formData, setFormData] = useState({
+    email: "",
+    senha: "",
+    telefone: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const { login } = useAuth();
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirect");
+
+  // Pega o campo do formulário que foi mexido
+  // e guarda o novo valor dentro de um objeto
+  // com todos os dados do formulário, sem apagar o que já estava lá.
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    setLoading(true);
+
+    try {
+      const response = await api.post("/usuario/login", formData);
+      const { token, usuario } = response.data;
+
+      // Usa a função do nosso AuthContext para gerenciar o estado
+      login(usuario, token);
+
+      // Se houver uma URL de redirecionamento, usa ela.
+      if (redirectUrl) {
+        router.push(redirectUrl);
+      } else {
+        // Caso contrário, redireciona com base no tipo de usuário.
+        const destination = usuario.is_prestador ? "/dashboard" : "/";
+        router.push(destination);
+      }
+    } catch (err) {
+      console.error("Erro no login", err);
+      setError(
+        err.response?.data?.message || "Ocorreu um erro durante o login."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    formData,
+    error,
+    loading,
+    handleChange,
+    handleSubmit,
+  };
+}
