d123440d03b8d9f2405d7d1ac1b8d9ba826dcea5
Washington <washington.contato1327@gmail.com>
2026-01-06 15:47:45 -0300

    Ajustes nos estilos do icone em cards da pagina torne-se um profissional

--- src/data/steps.js
+++ src/data/steps.js
@@ -8,37 +8,49 @@ import {
 } from "react-icons/pi";
 const steps = [
   {
-    icon: <PiNumberOneThin size={45} className="mb-2 border rounded-full" />,
-    title: "Cadastre-se como Profissional Jobberu",
+    icon: (
+      <PiNumberOneThin className="mb-2  p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
+    title: "Cadastre-se como Profissional",
     description:
       "Crie sua conta, adicione uma boa foto de perfil, escreva uma biografia clara e defina seu título profissional. Essas informações ajudam clientes a entenderem seu serviço e confiarem no seu trabalho.",
   },
   {
-    icon: <PiNumberTwoThin size={45} className="mb-2 border rounded-full" />,
+    icon: (
+      <PiNumberTwoThin className="mb-2 p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
     title: "Crie seu Serviço",
     description:
       "Adicione fotos de trabalhos realizados, escreva uma descrição objetiva e escolha a categoria correta. Você pode definir um valor inicial para ajudar o cliente a entender seu serviço.",
   },
   {
-    icon: <PiNumberThreeThin size={45} className="mb-2 border rounded-full" />,
+    icon: (
+      <PiNumberThreeThin className="mb-2  p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
     title: "Conexão Direta com o Cliente",
     description:
       "Na Jobberu, o contato é direto e privado. O cliente fala com você pelo WhatsApp para combinar valores, local, prazos e todos os detalhes do serviço.",
   },
   {
-    icon: <PiNumberFourThin size={45} className="mb-2 border rounded-full" />,
+    icon: (
+      <PiNumberFourThin className="mb-2  p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
     title: "Feche Negócio com Confiança",
     description:
       "O fechamento do serviço acontece de forma transparente. Perfis bem preenchidos, descrições claras e boas imagens aumentam suas chances de fechar novos trabalhos.",
   },
   {
-    icon: <PiNumberFiveThin size={45} className="mb-2 border rounded-full" />,
+    icon: (
+      <PiNumberFiveThin className="mb-2  p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
     title: "Construa sua Reputação",
     description:
       "Após o serviço, o cliente pode avaliar seu trabalho. Avaliações positivas aumentam sua credibilidade e ajudam outros clientes a escolherem você.",
   },
   {
-    icon: <PiNumberSixThin size={45} className="mb-2 border rounded-full" />,
+    icon: (
+      <PiNumberSixThin className="mb-2  p-2 rounded-full absolute -top-7 left-1/2 transform -translate-x-1/2 bg-green-500 text-6xl" />
+    ),
     title: "Cresça com Jobberu",
     description:
       "A Jobberu conecta profissionais locais a clientes da região, ajudando você a ganhar visibilidade, conquistar novos trabalhos e crescer de forma constante.",
